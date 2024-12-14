4b99bdfdac564a840fcf9f619d8dddac49793679
nrkimv6 <risingnrkim@outlook.kr>
2024-12-14 14:36:26 +0900

    add logs for background message

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,9 +5,13 @@ importScripts('./config/firebase-config-sw.js');
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// 백그라운드 메시지 처리
 messaging.onBackgroundMessage((payload) => {
     console.log("Background message received: ", payload);
+    console.log("Sender:", payload.from);
+    console.log("Priority:", payload.priority);
     const { title, body } = payload.notification;
-    self.registration.showNotification(title, { body });
+    self.registration.showNotification(title, { 
+        body,
+        timestamp: Date.now() // 알림 시간 기록
+    });
 });
